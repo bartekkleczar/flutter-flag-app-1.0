7a84a3a9d409e1b481d42f807597bef64025157c
Bartosz Klęczar <128930025+bartekkleczar@users.noreply.github.com>
2024-03-16 12:44:13 +0100

    theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flagg/main_screen.dart';
+import 'package:flagg/theme/theme_provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox('mybox');
 
-  runApp(MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeProvider(),
+    child: const MyApp(),
+  ));
 }
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flagg/data/flag/enum_colors.dart';
 import 'package:flagg/data/flag/enum_layout.dart';
+import 'package:flagg/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:provider/provider.dart';
 
 import 'data/database.dart';
 import 'data/flag/flag.dart';
@@ -36,6 +38,7 @@ class _MyApp extends State<MyApp> {
       print(i.name);
     }*/
     return MaterialApp(
+      theme: Provider.of<ThemeProvider>(context).themeData,
       debugShowCheckedModeBanner: false,
       home: Scaffold(),
     );
