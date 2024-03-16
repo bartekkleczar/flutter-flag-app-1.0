ff2274ee24d2f237321eb6524d0d11fb6cea2a80
Bartosz Klęczar <128930025+bartekkleczar@users.noreply.github.com>
2024-03-16 13:00:08 +0100

    UI starter

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,6 +1,5 @@
-import 'package:flagg/data/flag/enum_colors.dart';
-import 'package:flagg/data/flag/enum_layout.dart';
 import 'package:flagg/theme/theme_provider.dart';
+import 'package:flagg/widgets/flag_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:provider/provider.dart';
@@ -16,14 +15,12 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyApp extends State<MyApp> {
-
   final _myBox = Hive.box('mybox');
   FlaggDatabase db = FlaggDatabase();
 
   @override
-  void initState(){
-
-    if (_myBox.get('FLAGS') == null){
+  void initState() {
+    if (_myBox.get('FLAGS') == null) {
       db.createInitialData();
     } else {
       db.loadData();
@@ -37,10 +34,22 @@ class _MyApp extends State<MyApp> {
     /*for(var i in db.filter(Flag('', [FlagColor.black], FlagLayout.vertical, true), db.getFlags())){
       print(i.name);
     }*/
+    List<Flag> flags = db.getFlags();
     return MaterialApp(
       theme: Provider.of<ThemeProvider>(context).themeData,
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flagg Finder'),
+          centerTitle: true,
+        ),
+        body: ListView.builder(
+          itemCount: flags.length,
+          itemBuilder: (context, index){
+            return FlagTile(flag: flags[index]);
+          },
+        ),
+      ),
     );
   }
 }
---
+++ lib/widgets/flag_tile.dart
@@ -0,0 +1,20 @@
+import 'package:flagg/data/flag/flag.dart';
+import 'package:flutter/cupertino.dart';
+
+class FlagTile extends StatelessWidget {
+  Flag flag;
+
+  FlagTile({
+    super.key,
+    required this.flag,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+
+      ],
+    );
+  }
+}
